19e44326943dcace573f76eee0455c3a3d982688
tedliang <nightbear1009@gmail.com>
2018-06-10 00:12:43 +0800

    add db connect

--- index.js
+++ index.js
@@ -6,16 +6,41 @@ var connection = mysql.createConnection({
     user: 'root',
     password: 'passowrd',
     database: 'test_db'
-  });
+});
 
 
-  app = express();
+app = express();
 
-  global.app.get('/getHelloWorld', function (req, res) {
-      console.log("hello world");
-      res.json({
-          ReturnCode : "API0001"
-      });
-  });
+global.app.get('/getHelloWorld', function (req, res) {
+    console.log("hello world");
+    res.json({
+        ReturnCode: "API0001"
+    });
+});
 
-  app.listen(9900);
+global.app.get('/getDB', function (req, res) {
+    var connection = mysql.createConnection({
+        host: 'mysql',
+        port: 3306,
+        user: 'root',
+        password: 'passowrd',
+        database: 'test_db'
+    });
+    connection.connect();
+    connection.query('select * from googleplay', function (error, result, fields) {
+        if (error) {
+            res.json({
+                ReturnCode: "API0002",
+                Message: error
+            });
+
+        } else {
+            res.json({
+                ReturnCode: "API0001"
+            });
+        }
+    });
+    connection.end();
+});
+
+app.listen(9900);
